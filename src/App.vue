effabf8bad20a48446da5d22477591dd4459f462
ansike <ansike@zuoyebang.com>
2019-06-26 23:29:14 +0800

    test3

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    this is test!!
+    this is test!!!
     <router-view/>
   </div>
 </template>
