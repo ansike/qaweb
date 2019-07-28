b47d00afb58a77567a3aa16a63e948166631d828
ansike <ansike@zuoyebang.com>
2019-07-28 20:16:37 +0800

    test deploy

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    hhhh
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -9,6 +8,8 @@
     test qaweb
     test4
     test5
+    test6
+
     <router-view/>
   </div>
 </template>
