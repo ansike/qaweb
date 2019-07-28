8e66da8e72501c8bf822990a2c42fce12225b819
ansike <ansike@zuoyebang.com>
2019-07-28 20:12:43 +0800

    test deploy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,5 +14,10 @@ export default {
   components: {
     HelloWorld,
   },
+  mounted() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('get', 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx23f0e6c4c375f363&secret=6c7e3538ae351df909843b81d38122af');
+    xhr.send();
+  },
 };
 </script>
